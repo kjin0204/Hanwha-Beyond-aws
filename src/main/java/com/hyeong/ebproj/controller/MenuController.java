14e07101d9ccafee464bae97cd57f72fdfa38824
HyeonggyuKang <kin0204@naver.com>
2025-11-22 13:36:19 +0900

    first feature/test commit

---
+++ src/main/java/com/hyeong/ebproj/controller/MenuController.java
@@ -0,0 +1,13 @@
+package com.hyeong.ebproj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MenuController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm alive!";
+    }
+}
